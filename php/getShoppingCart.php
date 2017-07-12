ab6ee2f404e03abf0ae111d63a7789d6b797663d
caowanting <1506255424@qq.com>
2017-07-12 21:39:55 +0800

    收尾二

--- php/getShoppingCart.php
+++ php/getShoppingCart.php
@@ -1,7 +1,7 @@
 <?php
 	header("Content-Type:text/html;charset=utf-8");
+	$vipName   = $_REQUEST['userName'];
 	
-	$goodsId   = $_REQUEST['goodsId'];	
 	//2、数据保存在数据库中
 	//1）、建立连接（搭桥）
 	$conn = mysql_connect("localhost","root","qianfeng");
@@ -12,11 +12,15 @@
 	}
 	
 	//3）、传输数据（过桥）
-	$sqlstr = "select * from goodsInfo where goodsId='".$goodsId."'";
+	$sqlstr = "select * from goodsInfo g,shoppingCart s
+			   where g.goodsId = s.goodsId  and s.vipName = '".$vipName."'";
+	
 	$result = mysql_query($sqlstr,$conn);//执行查询的sql语句后，有返回值，返回的是查询结果
+		
 	if(!$result){
-		die("获取数据失败".mysql_error());
-	}		
+		die("SQL语句执行失败".mysql_error());
+	}
+			
 	//查询列数
 	 $query_cols = mysql_num_fields($result);
 	 //echo "列数：".$query_cols;
@@ -24,9 +28,10 @@
     $query_num =mysql_num_rows($result);
     //echo "行数：".$query_num;
 	
-	$str="";
+	$str="[";
 	
 	$query_row = mysql_fetch_array($result);//游标下移,拿出结果集中的某一行，返回值是拿到的行；
+	while($query_row){
 		$str = $str.'{"goodsId":"'.$query_row[0].'",
 		"goodsName":"'.$query_row[1].'",
 		"goodsType":"'.$query_row[2].'",
@@ -48,6 +53,14 @@
 		"beiyong12":"'.$query_row[18].'",
 		"beiyong13":"'.$query_row[19].'"
 		}';	
+		
+		
+		$query_row = mysql_fetch_array($result);
+		if($query_row){
+			$str = $str.",";
+		}
+	}
+	$str = $str."]";
 	//4、关闭数据库
 	mysql_close($conn);
 	
